a3673babeaf87e11b0dc4b9ab19642709684aa0b
MohdAinul <mohdainul01@gmail.com>
2026-03-27 01:59:46 +0530

    implement chat system

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config({});
 const cors = require("cors");
+const http = require("http");
 
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.json());
@@ -21,16 +22,20 @@ const authRouter = require("./src/routes/auth");
 const profileRouter = require("./src/routes/profile");
 const requestRouter = require("./src/routes/request");
 const userRouter = require("./src/routes/user");
+const initializeSocket = require("./src/utils/socket");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
+const server = http.createServer(app);
+initializeSocket(server);
+
 //database connect before server
 connectDB().then(() => {
   try {
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log(`Server running on ` + process.env.PORT);
     });
   } catch (error) {
@@ -41,5 +46,3 @@ connectDB().then(() => {
 app.get("/", (req, res) => {
   res.send("DevTinder Backend Working 🚀");
 });
-
-
